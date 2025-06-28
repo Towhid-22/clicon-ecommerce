fa598c5fa641478c04567e68f6f529529d7a7cd1
Towhidul Islam <towhid212037@gmail.com>
2025-06-28 18:42:31 +0600

    home page design done

--- src/components/allComponents/new-product/NewProduct.jsx
+++ src/components/allComponents/new-product/NewProduct.jsx
@@ -1,24 +1,29 @@
 import Container from "@/components/common/Container";
 import Flex from "@/components/common/Flex";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const NewProduct = () => {
   return (
     <Container>
       <Flex className={"gap-6 justify-between my-18"}>
-        <Image
-          src="/newProduct2.png"
-          width={648}
-          height={336}
-          alt="newProduct"
-        />
-        <Image
-          src="/newProduct1.png"
-          width={648}
-          height={336}
-          alt="newProduct"
-        />
+        <Link href="/">
+          <Image
+            src="/newProduct2.png"
+            width={648}
+            height={336}
+            alt="newProduct"
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            src="/newProduct1.png"
+            width={648}
+            height={336}
+            alt="newProduct"
+          />
+        </Link>
       </Flex>
     </Container>
   );
